54fba185cb25bb9f2e5764251f3665173cd35c33
maser <maser@lindsays-Macbook-Pro.local>
2019-12-20 10:51:56 +0700

    lindsay's website

---
+++ practice.js
@@ -0,0 +1,15 @@
+//for(var g = 0 < 5; g++;){
+    // console.log(g);
+//}
+
+//var x = 17;
+
+//console.log(x % 10);
+var my_array = [10, 11, 12, 13, 14];
+
+for(var i = 0; i< my_array.length; i++){
+    if(my_array [i]% 2 == 0);
+    console.log("even");
+}else{
+    console.log("odd");
+}
